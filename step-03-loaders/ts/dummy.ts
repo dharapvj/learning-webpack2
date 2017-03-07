f666057c5d7c27eceed59b95659cfe705826dc83
Vijay Dharap <dharapvj@gmail.com>
2017-03-08 00:40:39 +0530

    Step-03 now uses html template fragments. Step-04 to copy.

--- step-03-loaders/ts/dummy.ts
+++ step-03-loaders/ts/dummy.ts
@@ -8,7 +8,7 @@ export class FriendComponent {
     private friends: Friend[];
     constructor() {
         this.friends = [] as Friend[];
-        this.friends.push({id:4, name:'Nishant', location:'Pune'});
+        this.friends.push({id:4, name:'Nishant', location:'Pune (loaded via Typescript)'});
     }
     getFriends() {
         return this.friends;
